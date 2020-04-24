07f393709039c413243b1bb817dd81fcb2dbc4ad
SuperCode <supercode@SuperCodes-MacBook-Air.local>
2020-04-24 23:24:08 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+var z = 34 ;
+z= 67;
+
+console.log(z);
+
+let x= 34 ;
+x= 67;
+console.log(x);
+
+const y= 34 ;
+y= 67;
+console.log(y);
+
+/* Der Unterschied ist : 
+dass der zugewiesen wert 
+von ”const" nicht mehr geändert werden kann sowie bei 
+"var" oder "let" */
